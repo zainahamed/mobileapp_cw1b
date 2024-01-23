ca856197cdd91398b1fd504b9e7f6f96a4e2cb07
zainahamed <zain.ahamed99@gmail.com>
2024-01-23 22:11:15 +0530

    News are changes to links

--- lib/widgets/news_item_widget.dart
+++ lib/widgets/news_item_widget.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import '../models/news_model.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
+
 
 class NewsItemWidget extends StatelessWidget {
   final NewsModel newsItem;
@@ -9,42 +14,67 @@ class NewsItemWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.grey[100], // Slight grey background color
-      elevation: 2.0, // Add elevation for a card-like effect
-      margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
-      child: ListTile(
-        contentPadding: EdgeInsets.all(8.0),
-        leading: _buildNewsImage(newsItem.imageUrl),
-        title: Text(newsItem.title),
-        subtitle: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(height: 8.0),
-            Text(
-              'Published on: ${_formatPublishedDate(newsItem.publishedAt)}',
-              style: TextStyle(fontSize: 12.0),
+        color: Colors.grey[100], // Slight grey background color
+        elevation: 2.0, // Add elevation for a card-like effect
+        margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+        child: GestureDetector(
+          onTap: () {
+            _launchUrl(newsItem.url);
+          },
+          child: ListTile(
+            contentPadding: EdgeInsets.all(8.0),
+            leading: _buildNewsImage(newsItem.imageUrl),
+            title: Text(newsItem.title),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 8.0),
+                Text(
+                  'Published on: ${_formatPublishedDate(newsItem.publishedAt)}',
+                  style: TextStyle(fontSize: 12.0),
+                ),
+              ],
             ),
-          ],
-        ),
-        trailing: Row(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            IconButton(
-              icon: Icon(Icons.favorite_border),
-              onPressed: () {
-                // Implement logic to mark as favorite
-              },
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.favorite_border),
+                  onPressed: () {
+                    // Implement logic to mark as favorite
+                  },
+                ),
+                IconButton(
+                  icon: Icon(Icons.save),
+                  onPressed: () {
+                    // Implement logic to save the news
+                  },
+                ),
+              ],
             ),
-            IconButton(
-              icon: Icon(Icons.save),
-              onPressed: () {
-                // Implement logic to save the news
-              },
-            ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
+  }
+
+  Future<void> _launchUrl(String url) async {
+    try {
+      if (Platform.isAndroid) {
+        // For Android, try using the 'intent' URL scheme with Chrome, fallback to any browser
+        await launch('intent://$url#Intent;scheme=http;package=com.android.chrome;end');
+      } else {
+        // For iOS and others, use the 'http' scheme
+        await launch(url);
+      }
+    } on PlatformException catch (e) {
+      if (e.code == 'ACTIVITY_NOT_FOUND') {
+        // If Chrome is not available, try launching without specifying the browser package
+        await launch(url);
+      } else {
+        print('Error launching URL: $e');
+      }
+    } catch (e) {
+      print('Error launching URL: $e');
+    }
   }
 
   Widget _buildNewsImage(String? imageUrl) {
@@ -53,16 +83,16 @@ class NewsItemWidget extends StatelessWidget {
       height: 80.0,
       decoration: imageUrl != null
           ? BoxDecoration(
-        borderRadius: BorderRadius.circular(8.0),
-        image: DecorationImage(
-          image: NetworkImage(imageUrl),
-          fit: BoxFit.cover,
-        ),
-      )
+              borderRadius: BorderRadius.circular(8.0),
+              image: DecorationImage(
+                image: NetworkImage(imageUrl),
+                fit: BoxFit.cover,
+              ),
+            )
           : BoxDecoration(
-        color: Colors.grey[300],
-        borderRadius: BorderRadius.circular(8.0),
-      ),
+              color: Colors.grey[300],
+              borderRadius: BorderRadius.circular(8.0),
+            ),
     );
   }
 
@@ -78,5 +108,4 @@ class NewsItemWidget extends StatelessWidget {
       return 'N/A';
     }
   }
-
 }
